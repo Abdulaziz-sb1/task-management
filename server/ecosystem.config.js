29f2447a2f0ebc2d589e6a5e78e99fb034c9f6ef
Abdulaziz <Abdulaziz.bahasan@takamul.net.sa>
2024-10-16 17:45:17 +0300

    modifications in server

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+// EC2
+module.exports = {
+    apps: [
+        {
+            name: "task-management",
+            script: "npm",
+            args: "run dev",
+            env: {
+                NODE_ENV: 'development',
+            }
+        }
+    ]
+}
