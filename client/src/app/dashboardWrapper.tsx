8fedb89a9165693c40f4e3c47b2846606b17c79f
Abdulaziz <Abdulaziz.bahasan@takamul.net.sa>
2024-10-17 15:51:25 +0300

    added sign in and sign out

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,45 +1,48 @@
-'use client'
-import React, { useEffect } from 'react'
-import Navbar from '@/components/Navbar'
-import Sidebar from '@/components/Sidebar'
-import StoreProvider, { useAppSelector } from './redux'
+"use client";
+import React, { useEffect } from "react";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import AuthProvider from './authprovider'
+import StoreProvider, { useAppSelector } from "./redux";
 
-const DashboardLayout = ({ children }: {children: React.ReactNode}) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   // seperated file than layout.tsx to use 'use client'
   // const isSideBarCollapsed = useAppSelector(
   //   (state)=> state.global.isSidebarCollapsed,
   // )
-  const isDarkMode = useAppSelector(
-    (state)=> state.global.isDarkMode,
-  )
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
-  useEffect(()=>{
-    if(isDarkMode){
-      document.documentElement.classList.add('dark')
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
     } else {
-      document.documentElement.classList.remove('dark')
+      document.documentElement.classList.remove("dark");
     }
-  })
-  return <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-    {/* side bar */}
-    <Sidebar />
-    <main className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50
-      ${true ? "":"md:pl-64"}`}>
+  });
+
+  return (
+    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+      {/* side bar */}
+      <Sidebar />
+      <main
+        className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${true ? "" : "md:pl-64"}`}
+      >
         {/* navbar */}
         <Navbar />
-        { children } 
-
-    </main>
-  </div>
-}
-
-const DashboardWrapper = ({ children }:{children: React.ReactNode}) => {
-  return(
-    <StoreProvider>
-      <DashboardLayout>
         {children}
-      </DashboardLayout>
+      </main>
+    </div>
+  );
+};
+
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  
+  return (
+    <StoreProvider>
+      <AuthProvider>
+        <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
-  )
-}
-export default DashboardWrapper
+  );
+};
+export default DashboardWrapper;
